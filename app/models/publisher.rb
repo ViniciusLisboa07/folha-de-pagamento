c92bf220ce604b75db12c0ba3b4817c26182b8d2
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2023-06-29 21:22:16 -0300

    ajuste na fila

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -2,12 +2,12 @@
 
 class Publisher
   def publish_message(message)
-    @connection = Bunny.new(:host => "localhost", :vhost => "/", :user => "guest", :password => "guest")
+    @connection = Bunny.new(:host => "localhost", :vhost => "/", :durable => "true", :user => "guest", :password => "guest")
     @connection.start
     
     channel = @connection.create_channel
     puts " CRIOU"
-    queue = channel.queue('folha_pagamento')
+    queue = channel.queue('folha-pagamento')
     channel.default_exchange.publish(message, routing_key: queue.name)
     channel.close
     puts " [x] Sent 'Hello World!'"
